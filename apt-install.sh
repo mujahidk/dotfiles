7d2940f66fc4a75de7d2c9a5abf1a5ed5f610cd8
Mujahid Khaleel <mujahidkhaleel@gmail.com>
2020-09-16 15:24:53 -0400

    Add aliases and link files setup.

---
+++ apt-install.sh
@@ -0,0 +1,4 @@
+apt update
+apt install unzip zip gpg tmux \
+  python3 python3-venv \
+  make
--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ echo "Configuring dotfiles."
 
 ln -s ~/Projects/Personal/dotfiles/debian/bash_aliases ~/.bash_aliases
 ln -s ~/Projects/Personal/dotfiles/debian/bashrc ~/.bashrc
+ln -s ~/Projects/Personal/dotfiles/tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config/nvim/
+ln -s ~/Projects/Personal/dotfiles/vimrc ~/.config/nvim/init.vim
