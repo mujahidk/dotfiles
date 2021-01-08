b79139a881f91b110ed11819ac80260cb3d1a135
Mujahid Khaleel <mujahidkhaleel@gmail.com>
2021-01-08 10:30:37 -0500

    Add alacritty ln and history completion.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ echo "Configuring dotfiles."
 ln -s ~/Projects/Personal/dotfiles/debian/bash_aliases ~/.bash_aliases
 ln -s ~/Projects/Personal/dotfiles/debian/bashrc ~/.bashrc
 ln -s ~/Projects/Personal/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/Projects/Personal/dotfiles/alacritty.yml ~/.alacritty.yml
+ln -s ~/Projects/Personal/dotfiles/gitconfig ~/.gitconfig
 
 mkdir -p ~/.config/nvim/
 ln -s ~/Projects/Personal/dotfiles/vimrc ~/.config/nvim/init.vim
